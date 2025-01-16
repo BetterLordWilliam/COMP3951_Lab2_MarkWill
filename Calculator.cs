203719397b0a7ef67ef0b903d0f14e91630f5bed
BetterLordWilliam <willjordan05@icloud.com>
2025-01-16 14:24:48 -0800

    WO: Added calculator class.

--- Calculator.cs
+++ Calculator.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace COMP3951_Lab2_MarkWill
 {
-    public class Calculator
+    internal class Calculator
     {
+        struct Calculation
+        {
+            private string Calculation_Literal;
+            private float Calculation_Value;
+        };
+
+        private ArrayList past_calculations = new ArrayList();
+
         public Calculator()
         {
-
+             
         }
     }
 }
